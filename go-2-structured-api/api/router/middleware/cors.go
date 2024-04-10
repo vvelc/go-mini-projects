a4431bf03f57eb39862ceb0e30257cc0af56af79
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2024-04-10 12:10:51 -0400

    Add Cors middleware

---
+++ go-2-structured-api/api/router/middleware/cors.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/go-chi/cors"
+)
+
+func Cors(next http.Handler) http.Handler {
+	return cors.New(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"*"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	  }).Handler(next)
+}
