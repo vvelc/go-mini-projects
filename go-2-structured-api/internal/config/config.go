53f37df63437cd5957fc029b77cfbd0b97badb4d
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2024-04-10 12:11:17 -0400

    Upgrade Config management

--- go-2-structured-api/internal/config/config.go
+++ go-2-structured-api/internal/config/config.go
@@ -1,13 +1,34 @@
 package config
 
+import (
+	"github.com/spf13/viper"
+)
+
 type Config struct {
-	PORT int16
-	IS_DEBUG bool
+	Port    int16 `mapstructure:"PORT"`
+	IsDebug bool  `mapstructure:"DEBUG"`
 }
 
 var config *Config = &Config{
-	PORT: 3000,
-	IS_DEBUG: true,
+	Port:    3000,
+	IsDebug: true,
+}
+
+func LoadConfig() (c *Config, err error) {
+	viper.AddConfigPath(".")
+    viper.SetConfigName(".env")
+    viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+    err = viper.ReadInConfig()
+    if err != nil {
+        return
+    }
+
+    err = viper.Unmarshal(&config)
+
+    return config, err
 }
 
 func GetConfig() *Config {
