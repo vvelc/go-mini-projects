a631df063960a770f225eebca281991925889fa4
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2024-04-09 21:23:15 -0400

    Setup file logs

--- go-2-structured-api/util/logger/logger.go
+++ go-2-structured-api/util/logger/logger.go
@@ -7,13 +7,20 @@ import (
 )
 
 func New(isDebug bool) *zerolog.Logger {
+	runLogFile, _ := os.OpenFile(
+        "structured-api.log",
+        os.O_APPEND|os.O_CREATE|os.O_WRONLY,
+        0664,
+    )
+	multi := zerolog.MultiLevelWriter(os.Stdout, runLogFile)
+
 	logLevel := zerolog.InfoLevel
 	if isDebug {
 		logLevel = zerolog.TraceLevel
 	}
 
 	zerolog.SetGlobalLevel(logLevel)
-	logger := zerolog.New(os.Stdout).With().Timestamp().Logger()
+	logger := zerolog.New(multi).With().Timestamp().Logger()
 
 	return &logger
 }
